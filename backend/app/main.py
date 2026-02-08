3aa00b89cda7b373521fede4d44ad40e545813ac
Yash Sharma <yashsharma.aiml@gmail.com>
2026-02-08 20:33:40 +0530

    fix: wildcard CORS for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,13 +11,7 @@ def create_app() -> FastAPI:
 
   app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-      'http://localhost:5173',
-      'http://127.0.0.1:5173',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'https://prodzy.vercel.app', # replace with your Vercel URL
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
